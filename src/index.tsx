863a9cc0422785a15f8e3245cdd9401b6a422dd0
koderush <koderush@outlook.com>
2019-05-20 10:46:30 -0600

    edr version

--- src/index.tsx
+++ src/index.tsx
@@ -1,10 +1,63 @@
 import React from 'react';
-import ReactDOM from 'react-dom';
+import ReactDOM, {render} from 'react-dom';
 import './index.css';
-import App from './App';
+import './App.css';
 import * as serviceWorker from './serviceWorker';
 
-ReactDOM.render(<App />, document.getElementById('root'));
+interface EDRState {
+    product: string;
+    subtitle: string;
+    traces: string[];
+}
+
+class EDRComponent extends React.Component {
+    state: EDRState = {
+        product: "EDR",
+        subtitle: "Electronics Drilling Recorder",
+        traces: ["Bit Depth", "Flow"],
+    }
+    getTracesSize() {
+        return this.state.traces.length;
+    }
+
+    getTraceList() {
+        return (
+            <>
+                {this.state.traces.map((trace: string) => <li>{trace}</li>)}
+            </>
+        )
+    };
+
+    addTrace = () => {
+        this.setState({
+            traces: [...this.state.traces, "new trace"],
+        });
+    }
+
+    render() {
+        console.log("11001");
+        return (
+            <>
+                <h1>
+                    Product: {this.state.product}
+                </h1>
+                {this.state.subtitle && <h2>Subtitle: {this.state.subtitle}</h2>}
+                {this.getTracesSize() > 0 ? <p>We have {this.state.traces.length} traces</p> :
+                    <p>We have no traces.</p>}
+                <button id="add-trace" onClick={this.addTrace}>Add Trace</button>
+                <ol>{this.getTraceList()}</ol>
+            </>
+        )
+    };
+}
+
+    const pageContent = (
+        <div className="App">
+            <EDRComponent/>
+        </div>
+    );
+
+    ReactDOM.render(pageContent, document.getElementById('root'));
 
 // If you want your app to work offline and load faster, you can change
 // unregister() to register() below. Note this comes with some pitfalls.
